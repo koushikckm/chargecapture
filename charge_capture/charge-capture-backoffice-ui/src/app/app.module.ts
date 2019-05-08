89b99cf02f4f72b94064585fbf7d9496fc139e85
Koushik Sudheendra <koushik.sudheendra@healthasyst.com>
2019-05-08 11:45:19 +0530

    Components

--- charge_capture/charge-capture-backoffice-ui/src/app/app.module.ts
+++ charge_capture/charge-capture-backoffice-ui/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { PatientdetailComponent } from './patientdetail/patientdetail.component';
+import { PatientservicedetailComponent } from './patientservicedetail/patientservicedetail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PatientdetailComponent,
+    PatientservicedetailComponent
   ],
   imports: [
     BrowserModule
---
+++ charge_capture/charge-capture-backoffice-ui/src/app/patientservicedetail/patientservicedetail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-patientservicedetail',
+  templateUrl: './patientservicedetail.component.html',
+  styleUrls: ['./patientservicedetail.component.css']
+})
+export class PatientservicedetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
